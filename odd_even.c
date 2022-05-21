1247b6a464ce3d5cb00083e3db074d2e82fe7090
A kar <94515068+Akar0987@users.noreply.github.com>
2022-05-21 17:06:31 +0530

    Initial project version

---
+++ odd_even.c
@@ -0,0 +1,20 @@
+
+
+
+#include<stdio.h>
+int main()
+{
+    int num ;
+    printf("enter the number");
+    scanf("%d",&num);
+    if (num%2==0) {
+        printf("%d is a even number",num);
+    }
+    else{
+        printf("%d iz an odd number",num);
+    }
+
+
+
+ return 0;
+}
