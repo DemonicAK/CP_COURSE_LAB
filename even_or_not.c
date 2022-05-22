31790f0e3cd150fe30511ab4302dc8ee7b0fe998
Arijit Kar <arijitkar29@gmail.com>
2022-05-22 12:48:42 +0530

    22 May 1400

---
+++ even_or_not.c
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    int num ;
+    printf("enter the number");
+    scanf("%d",&num);
+    if (num%2==0) {
+        printf("%d is a even number",num);
+    }
+    
+return 0;
+    }
+
+
+
+
---
+++ multiply_two_numbers.c
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter two numbers:");
+    scanf("%d %d",&a,&b);
+    printf("%dx%d=%d",a,b,a*b);
+ return 0;
+}
